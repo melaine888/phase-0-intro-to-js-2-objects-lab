de8466c16e0df2af6e0e8a62e8f8057d5c28b167
Melaine Kanana <melaine.kanana@student.moringaschool.com>
2023-04-03 09:51:00 +0300

    DONE WITH ASSIGNMENT

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+const employee = {
+    name: "Melaine Kanana Mbae",
+    streetAddress: "339 embu 60100",
+  };
+
+  function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {
+        ...employee,
+        [key]: value
+      };
+    }
+
+    function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+        employee[key] = value;
+        return employee;
+      }
+
+    function deleteFromEmployeeByKey(employee, key) {
+        const newEmployee = { ...employee };
+        delete newEmployee[key];
+        return newEmployee;
+      }
+
+    function destructivelyDeleteFromEmployeeByKey(employee, key) {
+        delete employee[key];
+        return employee;
+      }
